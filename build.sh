1036c0deae249f30d994cf4ed4e9e032735a9fec
Eric Sirianni <eric.sirianni@gmail.com>
2024-03-14 14:17:28 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CGO_ENABLED=0 GOOS=linux go build -v -o statsd-firehose .
-#CGO_ENABLED=0 GOOS=darwin go build -v -o build/darwin-amd64/statsd-firehose .
+go build -v -o statsd-firehose .
